e769cef1c2e2b858b15f63a7792d692d060318bc
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-06-25 10:55:23 -0300

    create project

---
+++ src/main/java/com/tidz/cmdJpaCrud/CmdJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.cmdJpaCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdJpaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmdJpaCrudApplication.class, args);
+	}
+
+}
