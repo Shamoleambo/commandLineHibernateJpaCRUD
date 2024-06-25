64b7d6c9da2e1c8a7a4337bc89ba8847a641416c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-06-25 11:40:41 -0300

    Create Command Line Runner

--- src/main/java/com/tidz/cmdJpaCrud/CmdJpaCrudApplication.java
+++ src/main/java/com/tidz/cmdJpaCrud/CmdJpaCrudApplication.java
@@ -1,13 +1,22 @@
 package com.tidz.cmdJpaCrud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CmdJpaCrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CmdJpaCrudApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner runner(String[] args) {
+		return runner -> {
+			System.out.println("Hello Command Line Runner");
+		};
 	}
 
 }
